13dce06071b6a01dac918864c2b281859cbad236
vidhikaiwart <vidhikaiwart@gmail.com>
2025-07-19 08:49:08 -0500

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
+
 export default function About() {
+  return (
   
-    <div>
-      About
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'> About NestSeeker</h1>
+
+
+     
+      <p  className='mb-4 text-slate-700'>NestSeeker is a smart and intuitive 
+        platform designed to simplify the way people 
+        find and share living spaces. Whether you're 
+        relocating for work, starting your college journey, or simply seeking a better place to stay, NestSeeker connects you with verified spaces that match your lifestyle and budget. With powerful search filters, real-time listings, and a user-friendly interface, the platform ensures that your
+         housing search is smooth, secure, and stress-free.</ p >
+    
+    <p className='mb-4 text-slate-700'>What sets NestSeeker apart is its focus on both discovery and accessibility. Users can effortlessly explore spaces based on key preferences like location, pricing, amenities, and availability—all in just a few clicks. Property owners, on the other hand, get the tools to list and manage their spaces efficiently, reaching the right audience without hassle. Every profile is verified, and every listing is backed by 
+      quality standards to foster trust on both sides.</ p >
+    
+    <p className='mb-4 text-slate-700'>At its core, NestSeeker is more than just a rental platform—it’s a community builder. It bridges the gap between seekers and providers, making accommodation more accessible, transparent, and reliable. Whether you're moving to a new city or renting out your space, NestSeeker empowers you with a seamless digital experience, helping you find 
+      not just a place to stay, but a place to belong.</ p >
     </div>
-  
+  )  
 }
